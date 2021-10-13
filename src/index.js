b5565f968ba589a4d33537a05c2856b254ccaaae
jhernandez-11 <jhernandez11@protonmail.com>
2021-10-13 02:13:09 -0700

    sub menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+///////// Side Menu Controls /////////
 const sideMenu = document.querySelector(".header__nav--ion");
 let active = false;
 
@@ -39,4 +40,72 @@ window.addEventListener("resize", () => {
 
           active = false;
     }
+})
+
+///////// Sub Menu Controls /////////
+const subMenu1 = document.querySelector(".sub-menu1");
+const main = document.querySelector(".main")
+
+const nav1 = document.querySelector(".header__nav1")
+const nav2 = document.querySelector(".header__nav2")
+const nav3 = document.querySelector(".header__nav3")
+
+let active2 = false
+
+anime({
+    targets: '.sub-menu1',
+    translateY: -100
+})
+
+function asm(nav) {
+    const start = anime.timeline({
+        easing: 'easeInSine',
+    })
+    .add({
+        targets: '.sub-menu1',
+        translateY: 0,
+        duration: 0
+    })
+    .add({
+        targets: '.sub-menu1',
+        width: "100%",
+        easing: 'easeInSine',
+        opacity: 1,
+    })
+}
+
+function dasm(nav) {
+    const start = anime.timeline({
+        easing: 'easeOutSine',
+    })
+    .add({
+        targets: '.sub-menu1',
+        width: "0%",
+        easing: 'easeInSine',
+        opacity: 0,
+        
+    })
+    .add({
+        targets: '.sub-menu1',
+        translateY: -90,
+    })
+}
+
+nav1.addEventListener('mouseover', () => {
+    asm()
+    active2 = true
+})
+
+nav1.addEventListener('click', () => {
+    if (!active2) {
+        asm()
+        active2 = true
+    } else {
+        dasm()
+        active2 = false
+    }
+})
+main.addEventListener('click', () => {
+    dasm()
+    active2 = false
 })
